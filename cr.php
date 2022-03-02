715ca0335199f5140723ca11b1c4d1b667aa9dcc
ApolloXY <36796602+ApolloXY@users.noreply.github.com>
2022-03-02 21:42:41 +0530

    Update cr.php

--- cr.php
+++ cr.php
@@ -2,13 +2,13 @@
 
 $url = 'https://www.instagram.com/nasa/?__a=1';
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15") ); 
-curl_setopt($ch, CURLOPT_NOBODY, false);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result= curl_exec ($ch);
-curl_close ($ch);
-echo  $result ;
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json') );
+$data = curl_exec($ch);
+curl_close($ch);
+
+echo $data; // Show the response
 
 ?>
