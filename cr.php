59ba07c1a3ba291263c6b01e656b48c7fbdb704c
ApolloXY <36796602+ApolloXY@users.noreply.github.com>
2022-03-02 21:36:06 +0530

    Update cr.php

--- cr.php
+++ cr.php
@@ -1,21 +1,14 @@
 <?php
 
-// Script to test if the CURL extension is installed on this server
+$url = 'https://www.instagram.com/nasa/?__a=1';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, Array("User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.15) Gecko/20080623 Firefox/2.0.0.15") ); 
+curl_setopt($ch, CURLOPT_NOBODY, false);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result= curl_exec ($ch);
+curl_close ($ch);
+echo  $result ;
 
-// Define function to test
-function _is_curl_installed() {
-    if  (in_array  ('curl', get_loaded_extensions())) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-// Ouput text to user based on test
-if (_is_curl_installed()) {
-  echo "cURL is <span style=\"color:blue\">installed</span> on this server";
-} else {
-  echo "cURL is NOT <span style=\"color:red\">installed</span> on this server";
-}
 ?>
